4304aa86d604326edce9a4c722fd4a479c2cd503
蓝田_Loto <shorfng@126.com>
2022-02-11 19:09:44 +0800

    软件架构设计原则1 - 开闭原则（Open Close Principle，OCP）

---
+++ Java_2.0_Principle/src/main/java/com/loto/principle/a/OCP/ICourse.java
@@ -0,0 +1,19 @@
+package com.loto.principle.a.OCP;
+
+/**
+ * Author：蓝田_Loto
+ * Date：2019-07-05 13:11
+ * <p>PageName：ICourse.java
+ * </p>
+ * <p>Function：课程接口
+ */
+
+public interface ICourse {
+    Integer getId();
+
+    String getName();
+
+    Double getPrice();
+}
+
+
