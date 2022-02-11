9c5667c28972a11edb116fa185cea8d4c5f65678
蓝田_Loto <shorfng@126.com>
2022-02-11 19:09:58 +0800

    软件架构设计原则2 - 依赖倒置原则（Dependence Inversion Principle，DIP）

---
+++ Java_2.0_Principle/src/main/java/com/loto/principle/b/DIP/ICourse.java
@@ -0,0 +1,13 @@
+package com.loto.principle.b.DIP;
+
+/**
+ * Author：蓝田_Loto
+ * Date：2020-06-03 15:45
+ * <p>PageName：ICourse.java
+ * </p>
+ * <p>Function：课程接口
+ */
+
+public interface ICourse {
+	void study();
+}
